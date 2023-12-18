60675d8534caf4d39607d8c6e40dd69d77187fc8
arrzdev <andre.arroz.santos@gmail.com>
2023-12-18 00:05:37 +0000

    Added pthread flag to the make file and removed some prints

--- src/server/commands/command.cpp
+++ src/server/commands/command.cpp
@@ -2,12 +2,10 @@
 
 void Command::setupSocketConnection(int port, bool verbose, int socketfd, struct sockaddr_in serverInfo, struct sockaddr_in clientInfo){
   if (this->socketType == "UDP"){
-    printf("using udp socket\n");
     this->socket = make_unique<UdpSocket>(port, verbose, socketfd, serverInfo, clientInfo);
   }
 
   if (this->socketType == "TCP"){
-    printf("using tcp socket\n");
     this->socket = make_unique<TcpSocket>(port, verbose, socketfd, serverInfo, clientInfo);
   }
 }
--- src/server/commands/commandFactory.cpp
+++ src/server/commands/commandFactory.cpp
@@ -8,47 +8,47 @@ Command* CommandFactory::createCommand(string protocolMessage) {
   // catch exception throwed by the constructor, in that case the command is invalid and we return NULL 
   try {
     if (command == "LIN"){
-      printf("Creating login command\n");
+      // printf("Creating login command\n");
       return new Login(arguments);
     }
     else if (command == "LOU"){
-      printf("Creating logout command\n");
+      // printf("Creating logout command\n");
       return new Logout(arguments);
     }
     else if (command == "UNR"){
-      printf("Creating unregister command\n");
+      // printf("Creating unregister command\n");
       return new Unregister(arguments);
     }
     else if (command == "LMA"){
-      printf("Creating listmyauctions command\n");
+      // printf("Creating listmyauctions command\n");
       return new MyAuctions(arguments);
     }
     else if (command == "LMB"){
-      printf("Creating listmybids command\n");
+      // printf("Creating listmybids command\n");
       return new MyBids(arguments);
     }
     else if (command == "LST"){
-      printf("Creating list command\n");
+      // printf("Creating list command\n");
       return new List(arguments);
     }
     else if (command == "SRC"){
-      printf("Creating show record command\n");
+      // printf("Creating show record command\n");
       return new ShowRecord(arguments);
     }
     else if (command == "OPA"){
-      printf("Creating open auction command\n");
+      // printf("Creating open auction command\n");
       return new Open(arguments);
     }
     else if (command == "CLS"){
-      printf("Creating close auction command\n");
+      // printf("Creating close auction command\n");
       return new Close(arguments);
     }
     else if (command == "SAS"){
-      printf("Creating show asset command\n");
+      // printf("Creating show asset command\n");
       return new ShowAsset(arguments);
     }
     else if (command == "BID"){
-      printf("Creating bid command\n");
+      // printf("Creating bid command\n");
       return new Bid(arguments);
     }
   } catch(const std::exception& e ) {
